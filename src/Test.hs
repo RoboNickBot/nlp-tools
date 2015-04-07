fbb49df05dd9f5caa2cc3277d8f71713707214b7
RoboNickBot <nicklewchenko92@gmail.com>
2015-04-07 18:23:02 -0500

    added option for data-file root directory

--- src/Test.hs
+++ src/Test.hs
@@ -4,26 +4,40 @@ import NLP.Freq
 
 import NLP.Tools
 
+import Options.Applicative
 import System.Directory
 import qualified Data.List as L
 
 percentTestData = 10
 
-main = do targets <- (fmap (L.delete ".") . fmap (L.delete "..") 
-                      . getDirectoryContents) "/data/crubadan"
-          texts <- sequence (fmap qual targets)
-          let (ts,ps) = testprocess texts
-              
-              tf :: [(String, FreqList TriGram)]
-              tf = fmap (\(a,b) -> (a,features b)) ts
+rdir :: Parser String
+rdir = strOption ( long "directory" 
+                <> short 'd' 
+                <> metavar "DIRECTORY" 
+                <> help "Root directory of data files")
 
-              pf :: [(String, FreqList TriGram)]
-              pf = fmap (\(a,b) -> (a,features b)) ps
-              
-              res :: [(String, String)]
-              res = fmap (\(n,f) -> (n, choosebest pf f)) tf
+opts = info (helper <*> rdir) ( fullDesc 
+                             <> progDesc "Test identifier"
+                             <> header "analyze - test identifier")
 
-          sequence_ (fmap print res)
+main = execParser opts >>= testall
+
+testall dir = 
+  do targets <- (fmap (L.delete ".") . fmap (L.delete "..") 
+                 . getDirectoryContents) dir
+     texts <- sequence (fmap qual targets)
+     let (ts,ps) = testprocess texts
+
+         tf :: [(String, FreqList TriGram)]
+         tf = fmap (\(a,b) -> (a,features b)) ts
+
+         pf :: [(String, FreqList TriGram)]
+         pf = fmap (\(a,b) -> (a,features b)) ps
+
+         res :: [(String, String)]
+         res = fmap (\(n,f) -> (n, choosebest pf f)) tf
+
+     sequence_ (fmap print res)
 
 qual s = do t <- readFile ("/data/crubadan/" ++ s ++ "/SAMPSENTS")
             return (s,t)
